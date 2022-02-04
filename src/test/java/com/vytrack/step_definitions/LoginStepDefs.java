3d770fb452f014789a678e9817d2a9105c9bfb97
CydCoderKadircan <kadircangulde@gmail.com>
2022-02-04 09:30:19 +0300

    Tags added

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -14,12 +14,36 @@ public class LoginStepDefs {
     @When("the user enters the driver information")
     public void the_user_enters_the_driver_information() { //Skipped (Second step)  when there is no step definition
         System.out.println("I enter the credentials for driver");
-
     }
 
     @Then("the user should be able to login")
     public void the_user_should_be_able_to_login() { //Skipped (third step) when there is no step definition
-        System.out.println("As a driver, I logged in and verified the title changed to dashboard");
-
+        System.out.println("I logged in and verified the title changed to dashboard");
     }
+
+    @When("the user enters the sales manager information")
+    public void the_user_enters_the_sales_manager_information() {
+        System.out.println("I enter the credentials for sales manager");
+    }
+
+    @When("the user enters the store manager information")
+    public void the_user_enters_the_store_manager_information() {
+        System.out.println("I enter the credentials for store manager");
+    }
+
+    @When("the user enters the stock manager information")
+    public void the_user_enters_the_stock_manager_information() {
+        System.out.println("I enter the credentials for stock manager");
+    }
+
+    @When("the user enters the transportation manager information")
+    public void the_user_enters_the_transportation_manager_information() {
+        System.out.println("I enter the credentials for transportation manager");
+    }
+
+    @When("the user enters the product qualifier information")
+    public void the_user_enters_the_product_qualifier_information() {
+        System.out.println("I enter the credentials for product qualifier");
+    }
+
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,6 +1,38 @@
+@regression
 Feature: Users should be able to login
 
+  @execute_driver @smoke
   Scenario: Login as a driver
     Given the user is on the login page
     When the user enters the driver information
+    Then the user should be able to login
+
+  @execute_sales-manager
+  Scenario: Login as a sales manager
+    Given the user is on the login page
+    When the user enters the sales manager information
+    Then the user should be able to login
+
+  @execute_store-manager @smoke
+  Scenario: Login as a store manager
+    Given the user is on the login page
+    When the user enters the store manager information
+    Then the user should be able to login
+
+  @execute_stock-manager @smoke
+  Scenario: Login as a stock manager
+    Given the user is on the login page
+    When the user enters the stock manager information
+    Then the user should be able to login
+
+  @execute_transportation-manager @execute_driver @smoke
+  Scenario: Login as a transportation manager
+    Given the user is on the login page
+    When the user enters the transportation manager information
+    Then the user should be able to login
+
+  @execute_product-manager @execute_driver @smoke
+  Scenario: Login as a product qualifier
+    Given the user is on the login page
+    When the user enters the product qualifier information
     Then the user should be able to login
